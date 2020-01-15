4c90d3c6b386482c5ce4e2cb86b0ca2a7816f5be
B N <pyrooka@users.noreply.github.com>
2020-01-15 13:29:12 +0100

    Better HTTP error messages.

--- src/metabase/driver/cubejs/utils.clj
+++ src/metabase/driver/cubejs/utils.clj
@@ -38,16 +38,22 @@
         auth-token (if (nil? database) (get-cube-auth-token) (:authtoken (:details database)))]
     (loop []
       (log/debug "Request:" url auth-token query)
-      (let [resp (client/request {:method         :get
-                                  :url            url
-                                  :headers        {:authorization auth-token}
-                                  :query-params   {"query" query}
-                                  :accept         :json
-                                  :as             :json})]
-        (if (= (:status resp) 200) ; If the status is OK
-          (if (= (:error (:body resp)) "Continue wait") ; check does the response contains "Continue wait" message or not.
-            (do
-              (Thread/sleep 2000) ; If it contains, wait 2 sec,
-              (recur)) ; then retry the query.
-            resp)
-          resp)))))
+      (let [resp (client/request {:method            :get
+                                  :url               url
+                                  :headers           {:authorization auth-token}
+                                  :query-params      {"query" query}
+                                  :accept            :json
+                                  :as                :json
+                                  :throw-exceptions  false})
+            status-code           (:status resp)
+            body                  (:body resp)]
+        (case status-code
+          200 (if (= (:error body) "Continue wait") ; check does the response contains "Continue wait" message or not.
+                (do
+                  (Thread/sleep 2000) ; If it contains, wait 2 sec,
+                  (recur)) ; then retry the query.
+                resp)
+          400 (throw (Exception. (format "Error occured: %s" body)))
+          403 (throw (Exception. "Authorization error. Check your auth token!"))
+          500 (throw (Exception. (format "Internal server error: %s" body)))
+          :else (throw (Exception. (format "Unknown error. Body: %s" body))))))))
